7fdc61f0788e38b75996ae6e7f8be78469caf266
Mathew Dwyer <mdwyer139@gmail.com>
2025-02-22 11:53:17 +1100

    Finished project

--- main.py
+++ main.py
@@ -1,12 +1,35 @@
 from stats import count_words
 from stats import frequency
+from stats import sorted_frequency
+import sys
 
 def main():
-  with open("books/frankenstein.txt") as f:
+  if len(sys.argv) == 1:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
+  file = sys.argv[1]
+
+  with open(file) as f:
       file_contents = f.read()
       words_number = count_words(file_contents)
       letter_frequency = frequency(file_contents)
-      print(f'{words_number} words found in the document')
-      print(letter_frequency)
+      sorted_letters = sorted_frequency(letter_frequency)
+      # print(f'{words_number} words found in the document')
+      # print(letter_frequency)
+      print("============ BOOKBOT ============")
+      print(f'Analyzing book found at {file}...')
+      print("----------- Word Count ----------")
+      print(f'Found {words_number} total words')
+      print("--------- Character Count -------")
+      for letter in sorted_letters:
+        sort_key = ''
+        for key in letter:
+          sort_key = key
+        count = letter[key]
+        print(f'{key}: {count}')
+      print("============= END ===============")
+
+# file = "books/frankenstein.txt"
 
 main()
--- stats.py
+++ stats.py
@@ -13,5 +13,34 @@ def frequency(text):
       freq[character] = 1
   return freq
 
-# text = 'testing'
+def sort_on(dict):
+  sort_key = ''
+  for key in dict:
+    sort_key = key
+  # print(dict, sort_key)
+  return dict[sort_key]
+
+def sorted_frequency(frequency):
+  sorted = []
+  for freq in frequency:
+    if freq.isalpha():
+      dict = {freq: frequency[freq]}
+      sorted.append(dict)
+  # print(new_list)
+  sorted.sort(reverse=True, key=sort_on)
+  return sorted
+
+# text = 'testingaaa..'
+# freqs = frequency(text)
 # print(frequency(text))
+# print(sorted_frequency(freqs))
+# dict = {'t': 3}
+# print(dict[1])
+
+# sorted_letters = sorted_frequency(freqs)
+# for letter in sorted_letters:
+#   sort_key = ''
+#   for key in letter:
+#     sort_key = key
+#   count = letter[key]
+#   print(f'{key}: {count}')
